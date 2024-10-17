4befe615a8ad879945144c078f61ef037d5be301
reiop <redisolenmina1@gmail.com>
2024-10-17 13:44:00 +0300

    Create new Todo from user data

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser'
 
+import todoRoutes from './routes/todos.js'
+
 const app = express()
 app.use(bodyParser.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get("/json-test", (req, res) => {
     res.send({message: "json test ok"})
 });
+
+app.use("/todos", todoRoutes)
 
 app.listen(3009, () => {
     console.log("Server kuulab pordi 3009 peal")
